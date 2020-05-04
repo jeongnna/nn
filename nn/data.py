a8e4beee9c7a970872c25c48e52241fc92280708
Jeongmin Kim <jeongnna@Jeongminui-MacBookAir.local>
2020-05-04 17:12:53 +0900

    Update

--- nn/data.py
+++ nn/data.py
@@ -4,10 +4,15 @@ import numpy as np
 class Dataset:
 
     def __init__(self, data):
-        self.data = data # 튜플로 저장하는게 적당할 듯 (X, y) 이런 식
-        # 애초에 `data`에 튜플이 입력되길 기대하지만, 만약 ndarray라면 튜플로 한번 감싸주기
+        if isinstance(data, tuple):
+            for item in data:
+                assert isinstance(item, np.ndarray)
+                assert len(item) == len(data[0])
 
-        ## data 튜플 내의 array들 길이가 동일한지 검사
+        elif isinstance(data, np.ndarray):
+            data = (data,)
+
+        self.data = data
 
     def __iter__(self):
 
@@ -17,10 +22,10 @@ class Dataset:
         ptr = self._ptr
         n = self._batch_size
 
-        selected = self._indices[ptr:(ptr + n)]
-
-        if ptr < len(self):
+        if ptr < len(self._indices):
+            selected = self._indices[ptr:(ptr + n)]
             case = tuple(map(lambda x: x[selected], self.data))
+
             self._ptr += n
 
             return case
@@ -41,5 +46,9 @@ class Dataset:
 
         self._batch_size = batch_size
 
-        while self._ptr < len(self):
+        if drop_remainder:
+            num_remainder = len(self._indices) % batch_size
+            self._indices = self._indices[:-num_remainder]
+
+        while self._ptr < len(self._indices):
             yield next(self)
